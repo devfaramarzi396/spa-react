d98d3c863cc5c6626f7e01081ade0abfac2fb6bb
hfDev <dev.faramarzi369@gmail.com>
2023-09-21 01:52:48 +0430

    dynamic path

--- src/pages/Posts/Route.js
+++ src/pages/Posts/Route.js
@@ -1,13 +1,17 @@
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route ,useRouteMatch} from "react-router-dom"
 import IndexPosts from './Index.js';
 import ShowPost from './Show.js';
 
 
 const RoutePost = () => {
+    const {path}=useRouteMatch()
     return (
         <Switch>
-            <Route path="/posts" component={IndexPosts} exact />
-            <Route path="/posts/:postId" component={ShowPost} />
+            <Route path={path} component={IndexPosts} exact />
+            {/* <Route path="/posts" component={IndexPosts} exact /> */}
+            
+            <Route path={`${path}/:postId`} component={ShowPost} />
+            {/* <Route path="/posts/:postId" component={ShowPost} /> */}
         </Switch>
     )
 }
