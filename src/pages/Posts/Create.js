b4946d8c0f96bfc2e68a3e1b92cff9fbe3d372c2
hfDev <dev.faramarzi369@gmail.com>
2023-09-21 18:11:50 +0430

    validation , send form to DB

--- src/pages/Posts/Create.js
+++ src/pages/Posts/Create.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
 
 const CreatePost = () => {
-    const[title,setTitle]=useState('')
-    const[body,setBody]=useState('')
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title,body);
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
 
     }
     return (
@@ -16,16 +29,22 @@ const CreatePost = () => {
                     <div className="col-md-6">
                         <h3>Create Post :</h3>
 
-                        <form  onSubmit={(e) => handleSubmit(e)}>
+                        <form onSubmit={(e) => handleSubmit(e)}>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" className="form-label">Title</label>
-                                <input type="text" className="form-control" onChange={(e)=>setTitle(e.target.value)} />
+                                <input type="text" className="form-control" onChange={(e) => setTitle(e.target.value)} />
+                                <div className="form-text text-danger">
+                                    {title ? '' : 'title is required'}
+                                </div>
                             </div>
                             <div class="mb-3">
                                 <label className="form-label">Body</label>
-                                <textarea className="form-control" rows="3" onChange={(e)=>setBody(e.target.value)}></textarea>
+                                <textarea className="form-control" rows="3" onChange={(e) => setBody(e.target.value)}></textarea>
+                                <div className="form-text text-danger">
+                                    {body ? '' : "body is required"}
+                                </div>
                             </div>
-                            <button type="submit" className="btn btn-dark">Create</button>
+                            <button type="submit" className="btn btn-dark" disabled={title === '' || body === ''}>Create</button>
                         </form>
                     </div>
                 </div>
